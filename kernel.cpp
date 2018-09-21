2bd284b06b918a36866bb83e07306350ea01bfe3
boris-ns <b.sulicenko@gmail.com>
2018-09-21 14:42:32 +0200

    Init commit, created hello world os. It's possible to run it on virtualbox

---
+++ kernel.cpp
@@ -0,0 +1,33 @@
+#include "types.h"
+
+void printf(char* str)
+{
+    // GPU reads from address 0xb8000 what to write to the screen
+    // With variable type 'short' we group 2 bytes together
+    // First byte is of background/foreground (4 bits each), and other byte for character
+    uint16_t* VideoMemory = (uint16_t*)0xb8000;
+    
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        // Set character to low-byte of the element and keep old value of high-byte
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    }
+
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors()
+{
+    for (constructor* i = &start_ctors; i != &end_ctors; ++i)
+        (*i)();
+}
+
+extern "C" void kernelMain(void* multibootStruct, uint32_t magicNum)
+{
+    printf("Hello world");
+
+    while (true);
+}
